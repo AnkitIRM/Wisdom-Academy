2eca0dbd945bac04923bdafb11a18de1e63de916
unknown <nandgopalsinghbokaro@gmail.com>
2022-10-15 13:41:40 +0530

    open it

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,22 @@
 import '../styles/globals.css'
 import NavBar from '../components/NavBar'
+import NextNProgress from "nextjs-progressbar";
 
 function MyApp({ Component, pageProps }) {
   return (
-  <>
-    <NavBar />
-  <Component {...pageProps} />
+    <>
+      <NextNProgress
+        color="#933DC9"
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={3}
+        showOnShallow={true}
+        showSpinner={false} 
+      />
+      <NavBar />
+      <Component {...pageProps} />
 
-  </>
+    </>
   )
 }
 
--- pages/support.js
+++ pages/support.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import Image from 'next/image'
 
 const support = () => {
   return (
+    <>
     <div><h1>This page will be available soon</h1></div>
+    <div>
+
+
+    <div>
+      <Image src="/undraw_contact_us_re_4qqt.svg" width={500} height={500}></Image>
+    </div>
+    <div>
+      <input type="text" name="" id="" />
+      <input type="text" />
+      <input type="text" />
+      <button>hello</button>
+    </div>
+    </div>
+    </>
   )
 }
 
